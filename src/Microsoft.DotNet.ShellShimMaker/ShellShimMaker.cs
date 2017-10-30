5ad5ad1693bccd782e8dcd009553263b87da4fa1
William Li <wul@microsoft.com>
2017-10-29 20:39:01 -0700

    Add shell shim maker

--- src/Microsoft.DotNet.ShellShimMaker/ShellShimMaker.cs
+++ src/Microsoft.DotNet.ShellShimMaker/ShellShimMaker.cs
@@ -7,15 +7,55 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 using NuGet.Frameworks;
 
 namespace Microsoft.DotNet.ShellShimMaker
 {
     public class ShellShimMaker
     {
-        public ShellShimMaker()
-        {
+        private readonly string _systemPathToPlaceShim;
 
+        public ShellShimMaker(string systemPathToPlaceShim)
+        {
+            _systemPathToPlaceShim = systemPathToPlaceShim ?? throw new ArgumentNullException(nameof(systemPathToPlaceShim));
+        }
+
+        public void CreateShim(FileInfo packageExecutablePath, string shellCommandName)
+        {
+            var script = new StringBuilder();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                script.AppendLine("@echo off");
+                script.AppendLine($"dotnet exec {packageExecutablePath} %*");
+            }
+            else
+            {
+                throw new NotImplementedException("unix work in progress");
+            }
+
+            File.WriteAllText($"{GetScriptPath(shellCommandName)}", script.ToString());
+        }
+
+        public void Remove(string shellCommandName)
+        {
+            File.Delete(GetScriptPath(shellCommandName));
+        }
+
+        private string GetScriptPath(string shellCommandName)
+        {
+            var scriptPath = Path.Combine(_systemPathToPlaceShim, shellCommandName);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                scriptPath += ".cmd";
+            }
+            else
+            {
+                throw new NotImplementedException("unix work in progress");
+            }
+
+            return scriptPath;
         }
     }
 }
--- test/Microsoft.DotNet.ShellShimMaker.Tests/ShellShimMakerTests.cs
+++ test/Microsoft.DotNet.ShellShimMaker.Tests/ShellShimMakerTests.cs
@@ -2,23 +2,49 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using FluentAssertions;
-using FluentAssertions.Execution;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.Tools.Test.Utilities;
-using NuGet.Protocol.Core.Types;
 using Xunit;
 
 namespace Microsoft.DotNet.ShellShimMaker.Tests
 {
     public class ShellShimMakerTests : TestBase
     {
-        [Fact]
+        [WindowsOnlyFact]
         public void GivenAnExecutablePathItCanGenerateShimFile()
+        {
+            var outputDll = MakeHelloWorldExecutableDll();
+
+            var muxer = new Muxer();
+            var shellShimMaker = new ShellShimMaker(Path.GetDirectoryName(muxer.MuxerPath));
+            var shellCommandName = nameof(ShellShimMakerTests) + Path.GetRandomFileName();
+
+            shellShimMaker.CreateShim(
+                outputDll,
+                shellCommandName);
+            var stdOut = ExecuteInShell(shellCommandName);
+
+            stdOut.Should().Contain("Hello World");
+
+            // Tear down
+            shellShimMaker.Remove(shellCommandName);
+        }
+
+        private static string ExecuteInShell(string shellCommandName)
+        {
+            ExecuteAndCaptureOutput(new ProcessStartInfo
+            {
+                FileName = "CMD.exe",
+                Arguments = $"/C {shellCommandName}",
+                UseShellExecute = false
+            }, out var stdOut);
+            return stdOut ?? "";
+        }
+
+        private static FileInfo MakeHelloWorldExecutableDll()
         {
             const string target = "netcoreapp2.1";
             const string testAppName = "TestAppSimple";
@@ -30,51 +56,33 @@ namespace Microsoft.DotNet.ShellShimMaker.Tests
 
             var outputDll = testInstance.Root.GetDirectory("bin", configuration, target)
                 .GetFile($"{testAppName}.dll");
-
-            var muxer = new Muxer();
-            var shellShimMaker = new ShellShimMaker(muxer.MuxerPath);
-            var shellCommandName = Path.GetRandomFileName();
-            shellShimMaker.CreateShim(executablePath: outputDll, shellCommandName: shellCommandName);
-
-            var returnValue = ExecuteAndCaptureOutput(new ProcessStartInfo
-            {
-                FileName = shellCommandName,
-                UseShellExecute = false
-            }, out var stdOut, out var _);
-
-            returnValue.Should().Be(0);
-            stdOut.Should().Be("Hello World");
+            return outputDll;
         }
 
-        private static int ExecuteAndCaptureOutput(ProcessStartInfo startInfo, out string stdOut, out string stdErr) 
+        private static void ExecuteAndCaptureOutput(ProcessStartInfo startInfo, out string stdOut)
         {
             var outStream = new StreamForwarder().Capture();
-            var errStream = new StreamForwarder().Capture();
 
             startInfo.RedirectStandardOutput = true;
-            startInfo.RedirectStandardError = true;
 
             var process = new Process
             {
-                StartInfo = startInfo
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
             };
-
-            process.EnableRaisingEvents = true;
 
             process.Start();
 
             var taskOut = outStream.BeginRead(process.StandardOutput);
-            var taskErr = errStream.BeginRead(process.StandardError);
-
             process.WaitForExit();
 
             taskOut.Wait();
-            taskErr.Wait();
-
             stdOut = outStream.CapturedOutput;
-            stdErr = errStream.CapturedOutput;
+        }
 
-            return process.ExitCode;
+        [Fact(Skip = "Pending implementation")]
+        public void GivenAnExecutablePathThatRequiresPermission()
+        {
         }
     }
 }
