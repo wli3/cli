171fb07d853d2f4f59d8d56327dfc7f5491d3ff5
William Li <wul@microsoft.com>
2017-11-08 12:59:01 -0800

    XML deser

---
+++ src/Microsoft.DotNet.ExecutablePackageObtainer/ToolConfiguration.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.DotNet.ExecutablePackageObtainer
+{
+    public class ToolConfiguration
+    {
+        public ToolConfiguration(
+            string commandName,
+            string toolAssemblyEntryPoint,
+            EntryPointType entryPointType)
+        {
+            CommandName = 
+                commandName ?? throw new ArgumentNullException(nameof(commandName));
+            ToolAssemblyEntryPoint =
+                toolAssemblyEntryPoint ?? throw new ArgumentNullException(nameof(toolAssemblyEntryPoint));
+            EntryPointType = entryPointType;
+        }
+
+        public string CommandName { get; }
+        public string ToolAssemblyEntryPoint { get; }
+        public EntryPointType EntryPointType { get; }
+    }
+
+    public enum EntryPointType
+    {
+        DotnetNetCoreAssembly,
+        NativeBinary,
+        Script,
+    }
+}
---
+++ src/Microsoft.DotNet.ExecutablePackageObtainer/ToolConfigurationDeserializer.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.DotNet.Cli.Utils;
+using NuGet.Frameworks;
+
+namespace Microsoft.DotNet.ExecutablePackageObtainer
+{
+    public static class ToolConfigurationDeserializer
+    {
+        public static ToolConfiguration Deserialize(string pathToXml)
+        {
+            var serializer = new XmlSerializer(typeof(DotnetToolMetadata));
+
+            DotnetToolMetadata dotnetToolMetadata;
+
+            using (var fs = new FileStream(pathToXml, FileMode.Open))
+            {
+                var reader = XmlReader.Create(fs);
+                dotnetToolMetadata = (DotnetToolMetadata)serializer.Deserialize(reader);
+            }
+
+            var commandName = dotnetToolMetadata.CommandName;
+            var toolAssemblyEntryPoint = dotnetToolMetadata.ToolAssemblyEntryPoint;
+
+            EntryPointType entryPointType;
+            switch (dotnetToolMetadata.EntryPointType)
+            {
+                case DotnetToolMetadataEntryPointType.DotnetNetCoreAssembly:
+                    entryPointType = EntryPointType.DotnetNetCoreAssembly;
+                    break;
+                case DotnetToolMetadataEntryPointType.NativeBinary:
+                    entryPointType = EntryPointType.NativeBinary;
+                    break;
+                case DotnetToolMetadataEntryPointType.Script:
+                    entryPointType = EntryPointType.Script;
+                    break;
+                default:
+                    throw new Exception("TODO no checkin xml dersializtion failed, handle it outside!");
+            }
+
+            return new ToolConfiguration(commandName, toolAssemblyEntryPoint, entryPointType);
+        }
+    }
+}
