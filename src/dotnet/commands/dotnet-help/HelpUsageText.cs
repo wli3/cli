856aa973adb1284fcad9ded132a964dd83c356e2
William Li <wul@microsoft.com>
2018-03-21 15:20:39 -0700

    Reflect help text

--- src/dotnet/commands/dotnet-help/HelpUsageText.cs
+++ src/dotnet/commands/dotnet-help/HelpUsageText.cs
@@ -28,9 +28,7 @@ path-to-application:
   msbuild          {LocalizableStrings.MsBuildDefinition}
   vstest           {LocalizableStrings.VsTestDefinition}
   store            {LocalizableStrings.StoreDefinition}
-  install          {LocalizableStrings.InstallDefinition}
-  uninstall        {LocalizableStrings.UninstallDefinition}
-  update           {LocalizableStrings.UpdateDefinition}
+  tool             {LocalizableStrings.ToolDefinition}
   help             {LocalizableStrings.HelpDefinition}
 
 {LocalizableStrings.CommonOptions}:
@@ -53,6 +51,3 @@ runtime-options:
   --additional-deps <path>          {LocalizableStrings.AdditionalDeps}
 ";
 }
-
-
-//TODO wul no checkin refect dotnet tool here
