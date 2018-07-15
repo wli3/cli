3acde61d441e66fb41db136c7c195e9ee6a1f68d
William Li <wul@microsoft.com>
2018-07-15 10:15:11 -0700

    Add most test fofor greating directory

--- test/Microsoft.DotNet.Tools.Tests.Utilities.Tests/MockFileSystemTests.cs
+++ test/Microsoft.DotNet.Tools.Tests.Utilities.Tests/MockFileSystemTests.cs
@@ -19,8 +19,8 @@ namespace Microsoft.DotNet.Tools.Tests.Utilities.Tests
         public void DirectoryExistsShouldCountTheSameNameFile(bool testMockBehaviorIsInSync)
         {
             IFileSystem fileSystem = SetupSubjectFileSystem(testMockBehaviorIsInSync);
-            var directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
-            var nestedFilePath = Path.Combine(directroy, "filename");
+            string directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
+            string nestedFilePath = Path.Combine(directroy, "filename");
             fileSystem.File.CreateEmptyFile(nestedFilePath);
 
             fileSystem.Directory.Exists(nestedFilePath).Should().BeFalse();
@@ -33,13 +33,13 @@ namespace Microsoft.DotNet.Tools.Tests.Utilities.Tests
         {
             IFileSystem fileSystem = SetupSubjectFileSystem(testMockBehaviorIsInSync);
 
-            var directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
-            var nestedFilePath = $"{directroy}\\filename";
+            string directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
+            string nestedFilePath = $"{directroy}\\filename";
             fileSystem.File.CreateEmptyFile(nestedFilePath);
 
             fileSystem.File.Exists($"{directroy}/filename").Should().BeTrue();
         }
-        
+
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
@@ -47,13 +47,38 @@ namespace Microsoft.DotNet.Tools.Tests.Utilities.Tests
         {
             IFileSystem fileSystem = SetupSubjectFileSystem(testMockBehaviorIsInSync);
 
-            var directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
-            var nestedFilePath = Path.Combine(directroy, "filename");
+            string directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
+            string nestedFilePath = Path.Combine(directroy, "filename");
             fileSystem.File.CreateEmptyFile(nestedFilePath);
 
             fileSystem.File.Exists(nestedFilePath).Should().BeTrue();
         }
-        
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void CouldCreateDirectory(bool testMockBehaviorIsInSync)
+        {
+            IFileSystem fileSystem = SetupSubjectFileSystem(testMockBehaviorIsInSync);
+
+            string directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
+
+            fileSystem.Directory.Exists(directroy).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void ShouldNotThrowOnCreateDirectoryWhenExists(bool testMockBehaviorIsInSync)
+        {
+            IFileSystem fileSystem = SetupSubjectFileSystem(testMockBehaviorIsInSync);
+
+            string directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
+
+            Action a = () => fileSystem.Directory.CreateDirectory(directroy);
+            a.ShouldNotThrow();
+        }
+
         [WindowsOnlyTheory]
         [InlineData(false)]
         [InlineData(true)]
@@ -61,8 +86,8 @@ namespace Microsoft.DotNet.Tools.Tests.Utilities.Tests
         {
             IFileSystem fileSystem = SetupSubjectFileSystem(testMockBehaviorIsInSync);
 
-            var directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
-            var nestedFilePath = Path.Combine(directroy, "subfolder", "filename");
+            string directroy = fileSystem.Directory.CreateTemporaryDirectory().DirectoryPath;
+            string nestedFilePath = Path.Combine(directroy, "subfolder", "filename");
             Action a = () => fileSystem.File.CreateEmptyFile(nestedFilePath);
             a.ShouldThrow<DirectoryNotFoundException>();
         }
@@ -72,7 +97,7 @@ namespace Microsoft.DotNet.Tools.Tests.Utilities.Tests
             IFileSystem fileSystem;
             if (testMockBehaviorIsInSync)
             {
-                var temporaryFolder = new FileSystemMockBuilder
+                FileSystemMockBuilder temporaryFolder = new FileSystemMockBuilder
                 {
                     TemporaryFolder = Path.GetTempPath()
                 };
