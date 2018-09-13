873632c54fddab9efc2aec0163fda18da1cedb76
William Li <wul@microsoft.com>
2018-09-13 11:23:50 -0700

    WIP

---
+++ src/dotnet/ToolPackage/IManifestFileFinder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.DotNet.ToolPackage;
+using Microsoft.Extensions.EnvironmentAbstractions;
+using NuGet.Frameworks;
+using NuGet.Versioning;
+
+namespace Microsoft.DotNet.Cli.ToolPackage
+{
+    internal interface IManifestFileFinder
+    {
+        IEnumerable<(PackageId, NuGetVersion, NuGetFramework)> GetPackages(FilePath? manifestFilePath = null);
+    }
+}
