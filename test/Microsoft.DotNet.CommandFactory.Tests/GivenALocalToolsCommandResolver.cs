d03b67a932d6150229695936b3ab7ee2f789e33b
William Li <wul@microsoft.com>
2018-10-07 15:41:15 -0700

    WIP

---
+++ test/Microsoft.DotNet.CommandFactory.Tests/GivenALocalToolsCommandResolver.cs
@@ -0,0 +1,16 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using FluentAssertions;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+using Microsoft.DotNet.CommandFactory;
+
+namespace Microsoft.DotNet.Tests
+{
+
+    public class GivenALocalToolsCommandResolver : TestBase
+    {
+    }
+}
