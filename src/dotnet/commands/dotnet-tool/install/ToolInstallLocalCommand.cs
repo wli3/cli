bbc41c3e8d73fc6a7c262d016cc29b8febf98fcf
William Li <wul@microsoft.com>
2018-11-08 17:02:08 -0800

    WIP

---
+++ src/dotnet/commands/dotnet-tool/install/ToolInstallLocalCommand.cs
@@ -0,0 +1,90 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Transactions;
+using Microsoft.DotNet.Cli;
+using Microsoft.DotNet.Cli.CommandLine;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.Configurer;
+using Microsoft.DotNet.ShellShim;
+using Microsoft.DotNet.ToolPackage;
+using Microsoft.Extensions.EnvironmentAbstractions;
+using NuGet.Versioning;
+
+namespace Microsoft.DotNet.Tools.Tool.Install
+{
+
+    internal class ToolInstallLocalCommand : CommandBase
+    {
+        private readonly IReporter _reporter;
+        private readonly IReporter _errorReporter;
+
+        private readonly PackageId _packageId;
+        private readonly string _packageVersion;
+        private readonly string _configFilePath;
+        private readonly string _framework;
+        private readonly string[] _source;
+        private readonly string _verbosity;
+        private IEnumerable<string> _forwardRestoreArguments;
+
+        public ToolInstallLocalCommand(
+            AppliedOption appliedCommand,
+            ParseResult parseResult,
+            CreateToolPackageStoresAndInstaller createToolPackageStoreAndInstaller = null,
+            CreateShellShimRepository createShellShimRepository = null,
+            IEnvironmentPathInstruction environmentPathInstruction = null,
+            IReporter reporter = null)
+            : base(parseResult)
+        {
+            if (appliedCommand == null)
+            {
+                throw new ArgumentNullException(nameof(appliedCommand));
+            }
+
+            _packageId = new PackageId(appliedCommand.Arguments.Single());
+            _packageVersion = appliedCommand.ValueOrDefault<string>("version");
+            _configFilePath = appliedCommand.ValueOrDefault<string>("configfile");
+            _framework = appliedCommand.ValueOrDefault<string>("framework");
+            _source = appliedCommand.ValueOrDefault<string[]>("add-source");
+            _verbosity = appliedCommand.SingleArgumentOrDefault("verbosity");
+
+			_forwardRestoreArguments = appliedCommand.OptionValuesToBeForwarded();
+
+            _reporter = (reporter ?? Reporter.Output);
+            _errorReporter = (reporter ?? Reporter.Error);
+        }
+
+        public override int Execute()
+        {
+            if (_configFilePath != null && !File.Exists(_configFilePath))
+            {
+                throw new GracefulException(
+                    string.Format(
+                        LocalizableStrings.NuGetConfigurationFileDoesNotExist,
+                        Path.GetFullPath(_configFilePath)));
+            }
+
+
+            VersionRange versionRange = null;
+            if (!string.IsNullOrEmpty(_packageVersion) && !VersionRange.TryParse(_packageVersion, out versionRange))
+            {
+                throw new GracefulException(
+                    string.Format(
+                        LocalizableStrings.InvalidNuGetVersionRange,
+                        _packageVersion));
+            }
+
+            FilePath? configFile = null;
+            if (_configFilePath != null)
+            {
+                configFile = new FilePath(_configFilePath);
+            }
+
+            return 1;
+        }
+    }
+}
