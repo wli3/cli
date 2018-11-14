5b3c53ff1ee4540ecfa34e463f6507baaba95382
William Li <wul@microsoft.com>
2018-11-14 11:28:06 -0800

    Empty tests

--- test/Microsoft.DotNet.Cli.Utils.Tests/MarkOfTheWebDetectorTests.cs
+++ test/Microsoft.DotNet.Cli.Utils.Tests/MarkOfTheWebDetectorTests.cs
@@ -1,12 +1,30 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using Microsoft.DotNet.Tools.Test.Utilities;
 using Xunit;
 
 namespace Microsoft.DotNet.Cli.Utils.Tests
 {
     public class MarkOfTheWebDetectorTests
     {
+        [WindowsOnlyFact]
+        public void DetectFileWithMarkOfTheWeb()
+        {
+            throw new NotImplementedException();
+        }
 
+        [Fact]
+        public void NoFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        [NonWindowsOnlyFact]
+        public void WhenRunOnNonWindowsReturnFalse()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
