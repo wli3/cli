eb53b6e574c9fbcb9ef09a90ce924147365c4a53
William Li <wul@microsoft.com>
2018-11-14 16:41:26 -0800

    Add dotnet tool run parser

---
+++ test/dotnet.Tests/ParserTests/ToolRunParserTests.cs
@@ -0,0 +1,34 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+using FluentAssertions;
+using Microsoft.DotNet.Cli;
+using Microsoft.DotNet.Cli.CommandLine;
+using Xunit;
+using Xunit.Abstractions;
+using Parser = Microsoft.DotNet.Cli.Parser;
+
+namespace Microsoft.DotNet.Tests.ParserTests
+{
+    public class ToolRunParserTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public ToolRunParserTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void ListToolParserCanGetToolCommandNameArgument()
+        {
+            var result = Parser.Instance.Parse("dotnet tool run dotnetsay");
+
+            var appliedOptions = result["dotnet"]["tool"]["run"];
+            var packageId = appliedOptions.Arguments.Single();
+
+            packageId.Should().Be("dotnetsay");
+        }
+    }
+}
