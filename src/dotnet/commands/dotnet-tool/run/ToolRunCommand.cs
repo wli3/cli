4f10e9634fa58352bb4cfcd2a92ef54d442a66b1
William Li <wul@microsoft.com>
2018-11-14 16:36:50 -0800

    WIP dotnet tool run command parser

---
+++ src/dotnet/commands/dotnet-tool/run/ToolRunCommand.cs
@@ -0,0 +1,40 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.DotNet.Cli;
+using Microsoft.DotNet.Cli.CommandLine;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.Configurer;
+using Microsoft.DotNet.ToolPackage;
+using Microsoft.Extensions.EnvironmentAbstractions;
+
+namespace Microsoft.DotNet.Tools.Tool.Run
+{
+    internal class ToolRunCommand : CommandBase
+    {
+        public const string CommandDelimiter = ", ";
+        private readonly AppliedOption _options;
+        private readonly IReporter _reporter;
+        private readonly IReporter _errorReporter;
+
+        public ToolRunCommand(
+            AppliedOption options,
+            ParseResult result,
+            IReporter reporter = null)
+            : base(result)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+            _reporter = reporter ?? Reporter.Output;
+            _errorReporter = reporter ?? Reporter.Error;
+        }
+
+        public override int Execute()
+        {
+            return 0;
+        }
+    }
+}
