602254c5eec356bc821a60f4b434537ed2d17c2b
William Li <wul@microsoft.com>
2019-07-16 17:21:00 -0700

    Tests

---
+++ test/Microsoft.DotNet.ShellShim.Tests/OsxZshEnvironmentPathInstructionTests.cs
@@ -0,0 +1,104 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using FluentAssertions;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.Configurer;
+using Microsoft.DotNet.Tools;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Microsoft.Extensions.DependencyModel.Tests;
+using Microsoft.Extensions.EnvironmentAbstractions;
+using Moq;
+using Xunit;
+
+namespace Microsoft.DotNet.ShellShim.Tests
+{
+    public class OsxZshEnvironmentPathInstructionTests
+    {
+        [NonWindowsOnlyFact]
+        public void GivenPathNotSetItPrintsManualInstructions()
+        {
+            var reporter = new BufferedReporter();
+            var toolsPath = new BashPathUnderHomeDirectory("/home/user", ".dotnet/tools");
+            var pathValue = @"/usr/bin";
+            var provider = new Mock<IEnvironmentProvider>(MockBehavior.Strict);
+
+            provider
+                .Setup(p => p.GetEnvironmentVariable("PATH"))
+                .Returns(pathValue);
+            
+            provider
+                .Setup(p => p.GetEnvironmentVariable("SHELL"))
+                .Returns("/bin/bash");
+
+            var environmentPath = new OsxZshEnvironmentPathInstruction(
+                toolsPath,
+                reporter,
+                provider.Object,
+                FileSystemMockBuilder.Empty.File);
+
+            environmentPath.PrintAddPathInstructionIfPathDoesNotExist();
+
+            reporter.Lines.Should().Equal(
+                string.Format(
+                    CommonLocalizableStrings.EnvironmentPathOSXZshManualInstructions,
+                    toolsPath.Path));
+        }
+        
+        [NonWindowsOnlyTheory]
+        [InlineData("/home/user/.dotnet/tools")]
+        public void GivenPathSetItPrintsNothing(string toolsDirectoryOnPath)
+        {
+            var reporter = new BufferedReporter();
+            var toolsPath = new BashPathUnderHomeDirectory("/home/user", ".dotnet/tools");
+            var pathValue = @"/usr/bin";
+            var provider = new Mock<IEnvironmentProvider>(MockBehavior.Strict);
+
+            provider
+                .Setup(p => p.GetEnvironmentVariable("PATH"))
+                .Returns(pathValue + ":" + toolsDirectoryOnPath);
+
+            var environmentPath = new OsxZshEnvironmentPathInstruction(
+                toolsPath,
+                reporter,
+                provider.Object,
+                FileSystemMockBuilder.Empty.File);
+
+            environmentPath.PrintAddPathInstructionIfPathDoesNotExist();
+
+            reporter.Lines.Should().BeEmpty();
+        }
+        
+        [NonWindowsOnlyTheory]
+        [InlineData("~/.dotnet/tools")]
+        public void GivenPathSetItPrintsInstruction(string toolsDirectoryOnPath)
+        {
+            var reporter = new BufferedReporter();
+            var toolsPath = new BashPathUnderHomeDirectory("/home/user", ".dotnet/tools");
+            var pathValue = @"/usr/bin";
+            var provider = new Mock<IEnvironmentProvider>(MockBehavior.Strict);
+
+            provider
+                .Setup(p => p.GetEnvironmentVariable("PATH"))
+                .Returns(pathValue + ":" + toolsDirectoryOnPath);
+            
+            provider
+                .Setup(p => p.GetEnvironmentVariable("SHELL"))
+                .Returns("/bin/zsh");
+
+            var environmentPath = new OsxZshEnvironmentPathInstruction(
+                toolsPath,
+                reporter,
+                provider.Object,
+                FileSystemMockBuilder.Empty.File);
+
+            environmentPath.PrintAddPathInstructionIfPathDoesNotExist();
+
+            reporter.Lines.Should().Equal(
+                string.Format(
+                    CommonLocalizableStrings.EnvironmentPathOSXZshManualInstructions,
+                    toolsPath.Path));
+        }
+    }
+}
